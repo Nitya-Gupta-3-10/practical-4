d76b3c68150e65b3fbf6b2a7d078b9a081508e01
Nitya-Gupta-3-10 <nitya31007gupta@gmail.com>
2026-08-18 13:40:49 +0530

    Update hospital.sql

--- hospital.sql
+++ hospital.sql
@@ -1,3 +1,8 @@
 create database Hospital;
 use Hospital;
 create table patient (patient_id int primary key,name varchar(80) not null,dob varchar(60),gender varchar(50), address varchar(100) not null, blood_group varchar(40),phone_no varchar(80) unique not null);
+ create table doctor(doctor_id int primary key, name varchar(70),specialization varchar(80),phone_no varchar(60), email varchar(80), schedule varchar(70));
+alter table patient add doctor_id int;
+alter table patient add foreign key (doctor_id) references doctor(doctor_id);
+ create table appointment(appointment_id int primary key,patient_id int ,doctor_id int,date varchar(50), time varchar(50),foreign key (doctor_id) references doctor(doctor_id), foreign key (patient_id) references patient(patient_id)) ;
+alter table appointment add constraint unique_slot unique(date,time);
